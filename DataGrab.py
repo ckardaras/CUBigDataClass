4e2704410265018966c82847bb8cfdc36a95892c
Chris Kardaras <ckardaras92@gmail.com>
2021-03-19 14:09:28 -0600

    Update DataGrab.py

--- DataGrab.py
+++ DataGrab.py
@@ -3,6 +3,13 @@ import datetime
 
 def DataGrab(startDate,endDate):
     c = twint.Config()
+    config = twint.Config()
+    config.Proxy_host = "51.158.68.68"
+    config.Proxy_port = "8761"
+    config.Proxy_type = "http"
+    config.Username = username
+    twint.run.Lookup(config)
+    
     c.Popular_tweets = True
     c.Since = startDate
     c.Until = endDate
