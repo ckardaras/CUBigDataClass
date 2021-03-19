8ba97ce60582cc7a06c76af160af915bf5481abc
Chris Kardaras <ckardaras92@gmail.com>
2021-03-19 13:41:42 -0600

    Update DataGrab.py

--- DataGrab.py
+++ DataGrab.py
@@ -6,16 +6,15 @@ def DataGrab(startDate,endDate):
     c.Popular_tweets = True
     c.Since = startDate
     c.Until = endDate
-    c.Search = "#Bitcoin"
+    c.Search = "#Bitcoin OR $BTC"
     c.Custom["user"] = ["id", "username"]
     c.Limit = 10000
     c.Store_csv = True
     c.Output = startDate[0:10]
     twint.run.Search(c)
 
-startDate = datetime.datetime(2021,1,14)
-while startDate!=datetime.datetime(2021,3,14):
+startDate = datetime.datetime(2020,3,20)
+while startDate!=datetime.datetime(2021,3,16):
     endDate = startDate + datetime.timedelta(days=3)
-    print(startDate)
-    startDate += datetime.timedelta(days=1)
     DataGrab(str(startDate),str(endDate))
+    startDate += datetime.timedelta(days=1)
