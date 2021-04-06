8a136dd3db6ce94ad2e4d4f8edc586435612470b
rohansuri17 <rsuri@gloo.us>
2021-04-05 19:01:33 -0600

    Adding Updated Models. Need to work on Joins next. Removing Irrelevant Routes.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,49 @@
 from main import db
 
 
-class BTC(db.Model):
+
+class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    price = db.Column(db.DECIMAL)
+    date = db.Column(db.DateTime, nullable=False)
+    tweet = db.Column(db.Text, unique=True)
+    sentiment = db.Column(db.Float, nullable=False)
+
+class Coin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    bitcoin_avg_price = db.Column(db.Float, nullable=False)
+    bitcoin_day_high = db.Column(db.Float, nullable=False)
+    bitcoin_day_low = db.Column(db.Float, nullable=False)
+    ethereum_avg_price = db.Column(db.Float, nullable=False)
+    ethereum_day_high = db.Column(db.Float, nullable=False)
+    ethereum_day_low = db.Column(db.Float, nullable=False)
+
+class Date(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    week_id = db.Column(db.Integer)
+    month_id = db.Column(db.Integer)
+
+class Month(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    month_btc_avg = db.Column(db.Float, nullable=False)
+    month_btc_high = db.Column(db.Float, nullable=False)
+    month_btc_low = db.Column(db.Float, nullable=False)
+    month_btc_sent_avg = db.Column(db.Float, nullable=False)
+    month_eth_avg = db.Column(db.Float, nullable=False)
+    month_eth_high = db.Column(db.Float, nullable=False)
+    month_eth_low = db.Column(db.Float, nullable=False)
+    month_eth_sent_avg = db.Column(db.Float, nullable=False)
+
+class Week(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    week_btc_avg = db.Column(db.Float, nullable=False)
+    week_btc_high = db.Column(db.Float, nullable=False)
+    week_btc_low = db.Column(db.Float, nullable=False)
+    week_btc_sent_avg = db.Column(db.Float, nullable=False)
+    week_eth_avg = db.Column(db.Float, nullable=False)
+    week_eth_high = db.Column(db.Float, nullable=False)
+    week_eth_low = db.Column(db.Float, nullable=False)
+    week_eth_sent_avg = db.Column(db.Float, nullable=False)
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
 
-from app.models import BTC
+
 from main import app, db
 
 
@@ -9,10 +9,12 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/query')
+
+'''
 def query():
     new_row = BTC(price=5.3)
     db.session.add(new_row)
     db.session.commit()
     row = BTC.query.filter(BTC.id == 1).first()
     return render_template('query.html', row=row)
+'''
