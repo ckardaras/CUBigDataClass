7fa55b23e5e24f750d144e23cf4e5fed0e3d1cb1
Brendan <brendan.ostrom@colorado.edu>
2021-04-21 11:12:23 -0600

    "Fix" errors

--- app/routes.py
+++ app/routes.py
@@ -19,11 +19,11 @@ def chart():
 
 @app.route('/daily/sentiment_chart')
 def daily_sentiment_chart():
-    btc_prices = BTC_Daily.query.order_by(BTC_Daily.date).all()
-    tweets = db.session.query(int(func.avg(Tweet.sentiment).label('average')).group_by(Tweet.date).order_by(Tweet.date).all()
+    #btc_prices = BTC_Daily.query.order_by(BTC_Daily.date).all()
+    #tweets = db.session.query(int(func.avg(Tweet.sentiment).label('average')).group_by(Tweet.date).order_by(Tweet.date).all()
 
-    #return "te"
-    return render_template('daily/sentiment.html', btc=btc_prices, tweets=tweets)
+    return "te"
+    #return render_template('daily/sentiment.html', btc=btc_prices, tweets=tweets)
 
 '''
 def query():
