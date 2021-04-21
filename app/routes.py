9aeadd9bd17c9ac70bfb94636fb587c564f36d5b
Brendan <brendan.ostrom@colorado.edu>
2021-04-21 16:53:18 -0600

    Added new graphs

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from datetime import date
 from flask import render_template
 from sqlalchemy import func
 
-from app.models import BTC_Weekly, BTC_Daily, Tweet, BTC_Sentiments
+from app.models import BTC_Weekly, BTC_Daily, Tweet, BTC_Sentiments, BTC_Monthly
 from main import app, db
 
 
@@ -13,9 +13,24 @@ def home():
 
 
 @app.route('/btc/daily/price')
-def chart():
+def btc_daily_price():
     price = BTC_Daily.query.order_by(BTC_Daily.date).all()
-    return render_template('/bitcoin/daily/price.html', prices=price)
+    chart_title = "BTC Daily Price"
+    return render_template('/bitcoin/price.html', prices=price, title=chart_title)
+
+
+@app.route('/btc/weekly/price')
+def btc_weekly_price():
+    price = BTC_Weekly.query.order_by(BTC_Weekly.date).all()
+    chart_title = "BTC Weekly Price"
+    return render_template('/bitcoin/price.html', prices=price, title=chart_title)
+
+
+@app.route('/btc/monthly/price')
+def btc_monthly_price():
+    price = BTC_Monthly.query.order_by(BTC_Monthly.date).all()
+    chart_title = "BTC Monthly Price"
+    return render_template('/bitcoin/price.html', prices=price, title=chart_title)
 
 
 @app.route('/btc/daily/sentiment')
@@ -24,6 +39,7 @@ def daily_sentiment_chart():
     end = date(year=2021, month=3, day=16)
     btc_prices = BTC_Daily.query.filter(BTC_Daily.date >= start, BTC_Daily.date <= end).order_by(BTC_Daily.date).all()
     sentiments = BTC_Sentiments.query.order_by(BTC_Sentiments.date).all()
+    print("lol", sentiments)
 
     return render_template('/bitcoin/daily/sentiment.html', btc=btc_prices, sentiments=sentiments)
 
